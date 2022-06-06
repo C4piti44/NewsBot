8be7125ac2ce5b62f76d6a2adf3b968c9cc95087
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-06-06 19:12:52 +0300

    Update Code.py
    final

--- Code.py
+++ Code.py
@@ -4,7 +4,8 @@ import smtplib
 
 
 def n12():
-    source = requests.get("https://www.n12.co.il/").text
+    link = "https://www.n12.co.il/"
+    source = requests.get(link).text
     soup = BeautifulSoup(source , 'lxml')
     id = soup.find(id = "part1")
     place = 0
@@ -17,19 +18,25 @@ def n12():
         except:
             pass
         arr.append(str(place)+"." +headline)
+    arr.append(f'Link to the website : {link}')
+    arr.append(" ")
+    arr.append(" ")
     hey = '\n'.join(arr)
+
     return hey
 
 
 def weather():
-    source = requests.get("https://www.ynet.co.il/weather").text
+    link = "https://www.ynet.co.il/weather"
+    source = requests.get(link).text
     soup = BeautifulSoup(source, 'lxml')
-    weather = soup.select_one('div.forcast-tempature-mobile').text
+    weather = soup.select_one('div.forcast-tempature-mobile').text + f'Link to the website : {link}'
     return weather
 
 
 def bbc():
-    source = requests.get('https://www.bbc.com/').text
+    link ='https://www.bbc.com/'
+    source = requests.get(link).text
     soup = BeautifulSoup(source , 'lxml')
     media = soup.find('ul' , class_ = 'media-list')
     headline = media.find_all('li' , class_= 'media-list__item')
@@ -40,13 +47,15 @@ def bbc():
     for i  in range(1,5):
         arr.append(str(j) + "." + headline[i].a.text.strip())
         j=j+1
+    arr.append(f'Link to the website : {link}')
     hey = '\n'.join(arr)
     return hey
 
 
 def mariv():
+    link = 'https://www.maariv.co.il/'
     arr = []
-    source = requests.get('https://www.maariv.co.il/').text
+    source = requests.get(link).text
     soup = BeautifulSoup(source , 'lxml')
     top = "1." +soup.select_one('div.top-story-text-wrap').find_all('span')[1].text.strip() + " " +soup.select_one('div.top-story-text-wrap').find_all('span')[2].text
     article = soup.select("div.three-articles-in-row-text-wrap")
@@ -56,12 +65,12 @@ def mariv():
     for i in range(0,2):
         arr.append(str(j)+"."+article[i].find(class_ = "three-articles-in-row-overlay-text").text.strip() + article[i].find(class_ = "three-articles-in-row-title title-hover draft-title-cms").text.strip())
         j=j+1
-
+    arr.append(f'Link to the website : {link}')
     hey = '\n'.join(arr)
     return hey
 
 
-articles_array = ['The weather is : ' + weather() , " "," ", "Hey it's Mike the NewsBot here is your news for today,\nHave a great day! " ," ", n12() , bbc() ,' ',' ',  mariv()]
+articles_array = ['The weather is : ' + weather() , " "," ", "Hey, it's Mike the NewsBot here is your news for today,\nHave a great day! " ," ", n12() , bbc() ,' ',' ',  mariv()]
 message = '\n'.join(articles_array).encode('utf-8')
 sender_email = 'amit.gilat5@gmail.com'
 rec_email = 'amit.gilat5@gmail.com'
